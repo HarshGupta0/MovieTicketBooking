7ca7fd8149336064fe9a57abd7528d84eb709d94
harshgupta0 <hg09032@gmail.com>
2023-10-15 01:16:33 +0530

    Creadted Book Now page

--- lib/view/BookingPage.dart
+++ lib/view/BookingPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movieticket/model/DateAndTimeModel.dart';
+import 'package:movieticket/widgets/BookingCardItem.dart';
 
 class BookingPage extends StatefulWidget {
   final String imgname;
@@ -32,21 +33,38 @@ class BookingPageState extends State<BookingPage> {
               end: Alignment.bottomRight,
               colors: [Colors.green.shade600, Colors.black26])),
       child: Scaffold(
+        appBar: AppBar(
+         backgroundColor: Color(0xff618264).withOpacity(.9),
+          title: Text("Book Now"),
+          centerTitle: true,
+        ),
         backgroundColor: Colors.transparent,
         body: SingleChildScrollView(
           child: Column(
             children: [
-              OutlinedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/PaymentPage',arguments: Dateandtime("25-10-23","12:40","Lucknow, Inox"));
-                  },
-                  child: Row(
-                    children: [
-                      Text(" 25-10-23"),
-                      Text("12:40"),
-                      Text("Lucknow, Inox"),
-                    ],
-                  ))
+              Container(
+                margin: EdgeInsets.only(top: 35,left: 30,right: 30,bottom: 35),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height / 4,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  child: Image.asset(
+                    widget.imgname,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Center(child:Text(widget.name,style: TextStyle(color: Colors.grey,fontSize: 30,fontWeight: FontWeight.bold),) ,),
+              SizedBox(height: 20,),
+              BookingCardItem(imgname: widget.imgname, time:"12:40pm", loction:" Ghaziabad , Fun", date:"25-10-10"),
+              BookingCardItem(imgname: widget.imgname, time:"1:15 pm", loction:"Lucknnow , Inox", date:"27-10-10"),
+              BookingCardItem(imgname: widget.imgname, time:"2:45pm", loction:" Delhi, PVR", date:"26-10-10"),
+              BookingCardItem(imgname: widget.imgname, time:"3:00pm", loction:"Greater Noida , Fun", date:"28-10-10"),
+              BookingCardItem(imgname: widget.imgname, time:"12:40pm", loction:"Pune , Pvr", date:"30-10-10"),
+              BookingCardItem(imgname: widget.imgname, time:"10:00am", loction:" Mumbai , Inox", date:"18-10-10"),
+              SizedBox(height: 25,),
+              Center(child: Text("No MORE SHOWS",style: TextStyle(color: Colors.grey),),),
+              SizedBox(height: 25,),
             ],
           ),
         ),
---
+++ lib/widgets/BookingCardItem.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:movieticket/model/DateAndTimeModel.dart';
+class BookingCardItem extends StatefulWidget {
+  final String imgname;
+  final String time;
+  final String loction;
+  final String date;
+
+  BookingCardItem({
+    Key? key, // Use Key as a parameter if you want to provide a key
+    required this.imgname,
+    required this.time,
+    required this.loction,
+    required this.date,
+  }) : super(key: key);
+
+  @override
+  State<BookingCardItem> createState() => _BookingCardItemState();
+}
+
+class _BookingCardItemState extends State<BookingCardItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height / 10,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.green.shade600, Colors.black26])),
+      child: OutlinedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/PaymentPage',
+                arguments: Dateandtime(
+                    widget.date, widget.time, widget.loction));
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height / 10,
+                    child: ClipRRect(
+                      child: Image.asset(widget.imgname),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+              widget.date,
+                    style:
+                    TextStyle(color: Colors.grey, fontSize: 17),
+                  ),
+                  Text( widget.time,style:
+                  TextStyle(color: Colors.grey, fontSize: 17),),
+                  Text(widget.loction,style:
+                  TextStyle(color: Colors.grey, fontSize: 17),),
+                ],
+              ),
+              Icon(Icons.arrow_forward_ios_sharp,size: 35,color: Colors.grey,)
+            ],
+          )),
+    );
+  }
+}
